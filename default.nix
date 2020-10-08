7ed225557d1c62e5764d9a0ac1f85f0ce730ddab
Jeffrey David Johnson <jefdaj@protonmail.ch>
2020-10-08 14:13:22 -0700

    add (broken) star packages

--- default.nix
+++ default.nix
@@ -1,4 +1,4 @@
-let
+rec {
 
   # to update the the sha256sum:
   # nix-prefetch-url --unpack https://github.com/NixOS/nixpkgs-channels/archive/nixos-20.03.tar.gz
@@ -7,10 +7,19 @@ let
   in import (fetchFromGitHub {
     owner  = "nixos";
     repo   = "nixpkgs-channels";
-    rev    = "nixos-20.03";
+    rev    = "nixos-20.03"; # TODO pick an actual rev
     sha256 = "13qpa916qq1kqvfj8q4zkmnfnbh2kpx0nxxg04nblai0smz97820";
   }) {};
 
-  append-only-bb = nixpkgs.callPackage ./append-only-bb { inherit nixpkgs; };
+  # to build one of these, for example star-crypto:
+  # nix-build default.nix -A star-crypto
+  append-only-bb  = nixpkgs.callPackage ./append-only-bb  { inherit nixpkgs; };
+  star-crypto     = nixpkgs.callPackage ./star-crypto     { inherit nixpkgs; };
+  star-keygen     = nixpkgs.callPackage ./star-keygen     { inherit nixpkgs; };
+  star-terminal   = nixpkgs.callPackage ./star-terminal   { inherit nixpkgs; };
+  star-types      = nixpkgs.callPackage ./star-types      { inherit nixpkgs; };
+  star-util       = nixpkgs.callPackage ./star-util       { inherit nixpkgs; };
+  star-voter-db   = nixpkgs.callPackage ./star-voter-db   { inherit nixpkgs; };
+  star-controller = nixpkgs.callPackage ./star-controller { inherit nixpkgs; };
 
-in append-only-bb
+}
