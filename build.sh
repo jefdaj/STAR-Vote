61def26f230a71c703f5781a58deb5bcbbb29a8c
Jeffrey David Johnson <jefdaj@protonmail.ch>
2020-10-12 16:34:30 -0700

    tweak build dirs

--- build.sh
+++ build.sh
@@ -2,12 +2,14 @@
 
 # TODO get nix-build working, which will require updating the Arithmoi code
 
+dirs="append-only-bb star-controller star-crypto star-keygen star-terminal star-types star-util star-voter-db"
+
 # ...but but for now, build each one individually with Stack
-for d in append-only-bb star-*; do
+for d in $dirs; do
   pushd $d
 	while true; do stack build && break || sleep 10; done
 	popd
 done
 
 # and add all of them to the PATH
-export PATH=$(find append-only-bb star-* -type d -name bin | xargs realpath | while read d; do echo -n "${d}:"; done)$PATH
+export PATH=$(find $dirs -type d -name bin | xargs realpath | while read d; do echo -n "${d}:"; done)$PATH
